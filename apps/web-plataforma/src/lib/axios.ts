1b2d9eb7bd0f4e9478a1ed2e9550fa36b6188d68
claudiojas <claudiojasoares.dev@gmail.com>
2025-11-08 21:55:54 -0300

    add. configs to deploy

--- apps/web-plataforma/src/lib/axios.ts
+++ apps/web-plataforma/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001', // Usa variável de ambiente ou fallback local
   withCredentials: true,
 });
